bdb5682279c18ac98f0d080fbb0286b4883f41f4
esloan <emmansloan@gmail.com>
2025-02-12 23:05:13 -0800

    More SQL cheating

--- cbdc-custom-functions.php
+++ cbdc-custom-functions.php
@@ -28,148 +28,6 @@ function cbdc_init() {
 	add_action ( 'dp_duplicate_page', 'cbdc_duplicate_add_four_weeks', 10, 2 ); // this was priority 20, but there's a strange bug in WP that makes that not work with nested action hooks
 }
 
-/**
- * _parse_post_to_event is taken directly from     
- * all-in-one-event-calendar/app/model/event/creating.php
- * please update this function when the ai1ec plugin is updated
- */
-function parse_post_to_event($post_id) {
-	error_log("parsepost");
-	global $ai1ec_registry;
-	$all_day          = isset( $_POST['ai1ec_all_day_event'] )    ? 1                                                                 : 0;
-	$instant_event    = isset( $_POST['ai1ec_instant_event'] )    ? 1                                                                 : 0;
-	$timezone_name    = isset( $_POST['ai1ec_timezone_name'] )    ? sanitize_text_field( $_POST['ai1ec_timezone_name'] )              : 'sys.default';
-	$start_time       = isset( $_POST['ai1ec_start_time'] )       ? sanitize_text_field( $_POST['ai1ec_start_time'] )                 : '';
-	$end_time         = isset( $_POST['ai1ec_end_time'] )         ? sanitize_text_field( $_POST['ai1ec_end_time'] )                   : '';
-	$venue            = isset( $_POST['ai1ec_venue'] )            ? sanitize_text_field( $_POST['ai1ec_venue'] )                      : '';
-	$address          = isset( $_POST['ai1ec_address'] )          ? sanitize_text_field( $_POST['ai1ec_address'] )                    : '';
-	$city             = isset( $_POST['ai1ec_city'] )             ? sanitize_text_field( $_POST['ai1ec_city'] )                       : '';
-	$province         = isset( $_POST['ai1ec_province'] )         ? sanitize_text_field( $_POST['ai1ec_province'] )                   : '';
-	$postal_code      = isset( $_POST['ai1ec_postal_code'] )      ? sanitize_text_field( $_POST['ai1ec_postal_code'] )                : '';
-	$country          = isset( $_POST['ai1ec_country'] )          ? sanitize_text_field( $_POST['ai1ec_country'] )                    : '';
-	$google_map       = isset( $_POST['ai1ec_google_map'] )       ? 1                                                                 : 0;
-	$cost             = isset( $_POST['ai1ec_cost'] )             ? sanitize_text_field( $_POST['ai1ec_cost'] )                       : '';
-	$is_free          = isset( $_POST['ai1ec_is_free'] )          ? (bool)$_POST['ai1ec_is_free']                                     : false;
-	$ticket_url       = isset( $_POST['ai1ec_ticket_url'] )       ? sanitize_text_field( $_POST['ai1ec_ticket_url'] )                 : '';
-	$contact_name     = isset( $_POST['ai1ec_contact_name'] )     ? sanitize_text_field( $_POST['ai1ec_contact_name'] )               : '';
-	$contact_phone    = isset( $_POST['ai1ec_contact_phone'] )    ? sanitize_text_field( $_POST['ai1ec_contact_phone'] )              : '';
-	$contact_email    = isset( $_POST['ai1ec_contact_email'] )    ? sanitize_text_field( $_POST['ai1ec_contact_email'] )              : '';
-	$contact_url      = isset( $_POST['ai1ec_contact_url'] )      ? sanitize_text_field( $_POST['ai1ec_contact_url'] )                : '';
-	$show_coordinates = isset( $_POST['ai1ec_input_coordinates'] )? 1                                                                 : 0;
-	$longitude        = isset( $_POST['ai1ec_longitude'] )        ? sanitize_text_field( $_POST['ai1ec_longitude'] )                  : '';
-	$latitude         = isset( $_POST['ai1ec_latitude'] )         ? sanitize_text_field( $_POST['ai1ec_latitude'] )                   : '';
-	$cost_type        = isset( $_POST['ai1ec_cost_type'] )        ? sanitize_text_field( $_POST['ai1ec_cost_type'] )                  : '';
-	$rrule            = null;
-	$exrule           = null;
-	$exdate           = null;
-	$rdate            = null;
-
-
-	// if rrule is set, convert it from local to UTC time
-	if (
-		isset( $_POST['ai1ec_repeat'] ) &&
-		! empty( $_POST['ai1ec_repeat'] )
-	) {
-		$rrule = $_POST['ai1ec_rrule'];
-	}
-
-	// add manual dates
-	if (
-		isset( $_POST['ai1ec_exdate'] ) &&
-		! empty( $_POST['ai1ec_exdate'] )
-	) {
-		$exdate = $_POST['ai1ec_exdate'];
-	}
-	if (
-		isset( $_POST['ai1ec_rdate'] ) &&
-		! empty( $_POST['ai1ec_rdate'] )
-	) {
-		$rdate = $_POST['ai1ec_rdate'];
-	}
-
-	// if exrule is set, convert it from local to UTC time
-	if (
-		isset( $_POST['ai1ec_exclude'] ) &&
-		! empty( $_POST['ai1ec_exclude'] ) &&
-		( null !== $rrule || null !== $rdate ) // no point for exclusion, if repetition is not set
-	) {
-		$exrule = $ai1ec_registry->get( 'recurrence.rule' )->merge_exrule(
-			$_POST['ai1ec_exrule'],
-			$rrule
-		);
-	}
-
-	error_log($start_time);
-	$is_new = false;
-	try {
-		$event =  $ai1ec_registry->get(
-			'model.event',
-			$post_id ? $post_id : null
-		);
-		error_log("done trying");
-	} catch ( Ai1ec_Event_Not_Found_Exception $excpt ) {
-		error_log("catch");
-		// Post exists, but event data hasn't been saved yet. Create new event
-		// object.
-		$is_new = true;
-		$event  =  $ai1ec_registry->get( 'model.event' );
-	}
-	$formatted_timezone = $ai1ec_registry->get( 'date.timezone' )
-			->get_name( $timezone_name );
-	error_log($formatted_timezone);
-	if ( empty( $timezone_name ) || ! $formatted_timezone ) {
-		$timezone_name = 'sys.default';
-	}
-
-	unset( $formatted_timezone );
-	$start_time_entry = $ai1ec_registry
-		->get( 'date.time', $start_time, $timezone_name );
-	error_log($start_time_entry);
-	$end_time_entry   = $ai1ec_registry
-		->get( 'date.time', $end_time,   $timezone_name );
-
-	$timezone_name = $start_time_entry->get_timezone();
-	if ( null === $timezone_name ) {
-		$timezone_name = $start_time_entry->get_default_format_timezone();
-	}
-
-	$event->set( 'post_id',          $post_id );
-	$event->set('start-time', $start_time);
-	$event->set( 'start',            $start_time_entry );
-	if ( $instant_event ) {
-		$event->set_no_end_time();
-	} else {
-		$event->set( 'end',           $end_time_entry );
-		$event->set( 'instant_event', false );
-	}
-	$event->set( 'timezone_name',    $timezone_name );
-	$event->set( 'allday',           $all_day );
-	$event->set( 'venue',            $venue );
-	$event->set( 'address',          $address );
-	$event->set( 'city',             $city );
-	$event->set( 'province',         $province );
-	$event->set( 'postal_code',      $postal_code );
-	$event->set( 'country',          $country );
-	$event->set( 'show_map',         $google_map );
-	$event->set( 'cost',             $cost );
-	$event->set( 'is_free',          $is_free );
-	$event->set( 'ticket_url',       $ticket_url );
-	$event->set( 'contact_name',     $contact_name );
-	$event->set( 'contact_phone',    $contact_phone );
-	$event->set( 'contact_email',    $contact_email );
-	$event->set( 'contact_url',      $contact_url );
-	$event->set( 'recurrence_rules', $rrule );
-	$event->set( 'exception_rules',  $exrule );
-	$event->set( 'exception_dates',  $exdate );
-	$event->set( 'recurrence_dates', $rdate );
-	$event->set( 'show_coordinates', $show_coordinates );
-	$event->set( 'longitude',        trim( $longitude ) );
-	$event->set( 'latitude',         trim( $latitude ) );
-	$event->set( 'ical_uid',         $event->get_uid() );
-
-	return $event;
-}
-
 
 /**
  * Add custom columns (ai1ec date, caller, band) to the All Events listing
@@ -323,6 +181,7 @@ function cbdc_add_quick_edit($column_name, $post_type) {
  * Save data from the Quick Edit to the database.
  */
 function cbdc_save_quick_edit_data($post_id, $post, $update) {
+
 	// verify if this is an auto save routine. If it is, our form has not been submitted, so exit
 	if (defined ( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE)
 		return $post_id;
@@ -354,11 +213,8 @@ function cbdc_save_quick_edit_data($post_id, $post, $update) {
 	if (isset ( $_POST ['ai1ec_event_date'] )) {
 		$ai1ec_event_date_entered = $_POST ['ai1ec_event_date'];
 		// this date is at midnight
-		$entered_datetime = new DateTime ( $ai1ec_event_date_entered );
-		// datetime as measured in unix time
-		$entered_date = $entered_datetime->format ( 'U' );
 
-		$event = parse_post_to_event($post_id);
+		//$event = parse_post_to_event($post_id);
 
 
 		// Lacking a better way to get the original start and end date and time
@@ -366,28 +222,37 @@ function cbdc_save_quick_edit_data($post_id, $post, $update) {
 		// into the database
 		global $wpdb;
 		$query = $wpdb->prepare("SELECT * FROM {$wpdb->prefix}ai1ec_events WHERE post_id = %s", $post_id);
-		error_log($query);
 		$event_row = $wpdb->get_row($query);
-		error_log(json_encode($event_row));
-		$old_start_date = $event_row->start;
 
-		$tz_offset = -6 * 3600;
+		/*
+		 * Get entered date, preserve the old time, and modify both the start and end of the ai1ec event, leaving the same time in place. Example: an event originally scheduled for February 2nd from 7-9:30 pm is cloned using the Events menu. When the date is changed from 2/2/2018 to 3/2/2018, the new start time should be 3/2/2018 at 7 pm and the end time 3/2/2018 at 9:30 pm.
+		 */
+		$tz = new DateTimeZone($event_row->timezone_name);
+		$entered_datetime = new DateTimeImmutable($ai1ec_event_date_entered, $tz);
+		$old_start_datetime = (new DateTimeImmutable("", $tz))->setTimestamp($event_row->start);
 
-		// THIS IS A BUG: the offset is to the current time not the entered time
-		$old_start_time_offset = ($old_start_date + $tz_offset) % 86400 - $tz_offset; // get the time offset from midnight of the old date
-		error_log($old_start_time_offset);
-		$new_start_date = $entered_date + $old_start_time_offset; // add the time offset
-		$event->set('start', $new_start_date); 
-
-		$old_end_date = $event_row->end;
-		$old_end_time_offset = ($old_end_date + $tz_offset) % 86400 - $tz_offset; // get the time offset from midnight of the old date
-		$new_end_date = $entered_date + $old_end_time_offset; // add the time offset
-		$event->set('end', $new_end_date); // ai1ec stores GMT
-		error_log("750");
-
-		do_action( 'ai1ec_save_post', $event );
-		$event->save ( TRUE ); // TRUE means update, don't create new
-
+		$hour = intval($old_start_datetime->format("H"));
+		$minute = intval($old_start_datetime->format("i"));
+		$new_start_datetime = $entered_datetime->setTime($hour, $minute); // add the time offset
+		$new_start_timestamp = $new_start_datetime->getTimestamp(); // ai1ec stores GMT
+		//$event->set('start', $new_start_timestamp); 
+		
+		$old_end_datetime = (new DateTimeImmutable("", $tz))->setTimestamp($event_row->end);
+		$old_duration = $old_start_datetime->diff($old_end_datetime);
+		$new_end_datetime = $new_start_datetime->add($old_duration);
+		$new_end_timestamp = $new_end_datetime->getTimestamp(); // ai1ec stores GMT
+		//$event->set('end', $new_end_timestamp); // ai1ec stores GMT
+		
+		//do_action( 'ai1ec_save_post', $event );
+		//$event->save ( TRUE ); // TRUE means update, don't create new
+		$wpdb->update(
+			"{$wpdb->prefix}ai1ec_events",
+			array("start" => $new_start_timestamp, "end" => $new_end_timestamp),
+			array("post_id" => $post_id),
+			"%d",
+			"%d",
+		);
+		                       
 		// reset the cache
 		//$ai1ec_events_helper->delete_event_cache ( $post_id );
 		//$ai1ec_events_helper->cache_event ( $event );
@@ -395,8 +260,6 @@ function cbdc_save_quick_edit_data($post_id, $post, $update) {
 		// LABEL:magicquotes
 		// restore `magic` WordPress quotes to maintain compatibility
 		$_POST = add_magic_quotes( $_POST );
-
-		return $event;
 	}
 }
 function cbdc_enqueue_edit_scripts() {
